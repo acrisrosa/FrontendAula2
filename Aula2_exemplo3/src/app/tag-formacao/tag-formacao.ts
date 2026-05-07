ddf9be52b7b670272bb5fec733b6b08ab4b71c35
acrisrosa <cris2022pereira@gmail.com>
2026-05-07 15:37:15 +0100

    Exemplo3 da aula 2 de frontend

---
+++ Aula2_exemplo3/src/app/tag-formacao/tag-formacao.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-formacao',
+  imports: [],
+  templateUrl: './tag-formacao.html',
+  styleUrl: './tag-formacao.css',
+})
+export class TagFormacao {}
