7aed2d22df48aab181462184963d00b1e4449d48
acrisrosa <cris2022pereira@gmail.com>
2026-05-12 22:52:33 +0100

    Fim da aula  12maio

--- Aula2_exemplo3/src/app/tag-listas/tag-listas.ts
+++ Aula2_exemplo3/src/app/tag-listas/tag-listas.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   templateUrl: './tag-listas.html',
   styleUrl: './tag-listas.css',
 })
-export class TagListas {}
+export class TagListas {
+
+  minhaString: string = 'Paulo';
+
+  minhaLista: string[] =['Tony', 'data_pipe_default_option', 'samata', 'leonardo', 'priscila', 'sara','1','false', this.minhaString,];
+  
+  minhaListaJS: any[] =["Tony", "data_pipe_default_option", "samata", "leonardo", "priscila", "sara",1,false];
+ 
+  //minhaListaJSNumer: number[] =["Tony", "data_pipe_default_option", "samata", "leonardo", "priscila", "sara","1","false"];
+}
